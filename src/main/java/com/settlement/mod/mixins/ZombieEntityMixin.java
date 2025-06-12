756a328dc496e8bd7d6342d35bb2feb1629916d4
jlzht <jl.zht@proton.me>
2025-06-11 22:49:40 -0300

    feat: added mixin

--- src/main/java/com/settlement/mod/mixins/ZombieEntityMixin.java
+++ src/main/java/com/settlement/mod/mixins/ZombieEntityMixin.java
@@ -15,15 +15,36 @@ import net.minecraft.entity.mob.MobEntity;
 import net.minecraft.entity.ai.goal.ActiveTargetGoal;
 
 import net.minecraft.entity.mob.HostileEntity;
+import net.minecraft.entity.mob.VindicatorEntity;
+import net.minecraft.entity.mob.AbstractSkeletonEntity;
+import net.minecraft.entity.mob.IllagerEntity;
+import net.minecraft.entity.mob.VexEntity;
+import net.minecraft.entity.mob.VindicatorEntity;
+import net.minecraft.entity.mob.WitchEntity;
+import net.minecraft.entity.mob.RavagerEntity;
+import net.minecraft.entity.mob.IllusionerEntity;
+import net.minecraft.entity.mob.PatrolEntity;
+import net.minecraft.entity.mob.PillagerEntity;
 import org.slf4j.Logger;
 import com.mojang.logging.LogUtils;
 
-@Mixin(ZombieEntity.class)
+@Mixin({
+        AbstractSkeletonEntity.class,
+        ZombieEntity.class,
+        IllagerEntity.class,
+        IllusionerEntity.class,
+        PatrolEntity.class,
+        PillagerEntity.class,
+        RavagerEntity.class,
+        VexEntity.class,
+        VindicatorEntity.class,
+        WitchEntity.class
+})
 public abstract class ZombieEntityMixin extends HostileEntity {
         protected ZombieEntityMixin(EntityType<? extends ZombieEntity> entityType, World world) {
                 super(entityType, world);
         }
-        @Inject(method = "Lnet/minecraft/entity/mob/ZombieEntity;initCustomGoals()V", at = @At("HEAD"), cancellable = true)
+        @Inject(method = "initGoals", at = @At("TAIL"))
 	      private void initGoals(CallbackInfo info) {
           this.targetSelector.add(3, new ActiveTargetGoal<AbstractVillagerEntity>(this, AbstractVillagerEntity.class, true));
         }
